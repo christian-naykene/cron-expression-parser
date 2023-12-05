571fe99d9475392a15261aacea595fa1b306fc0b
Christian Naykene <younchriss@live.co.uk>
2023-12-05 09:25:39 +0000

    implement cronParser and test

--- cron-parser.js
+++ cron-parser.js
@@ -5,16 +5,72 @@
 
 // 1. Parse the cron string
 const cronParser = (cronString) => {
-  const [minute, hour, dayOfMonth, month, dayOfWeek, command] =
+  if (!cronString) {
+    console.log("No cron string found");
+    process.exit(1);
+  }
+
+  const [minute, hour, dayOfMonth, month, dayOfWeek, ...command] =
     cronString.split(/\s+/);
-  return {
-    minute,
-    hour,
-    dayOfMonth,
-    month,
-    dayOfWeek,
-    command,
+
+  //2. Expand each field
+  const expandField = (field, max) => {
+    if (!field) {
+      return [];
+    }
+
+    if (field === "*") {
+      const result = [];
+      for (let i = 1; i <= max; i++) {
+        result.push(i);
+      }
+      return result;
+    }
+
+    if (field.includes(",")) {
+      return field.split(",").map((value) => parseInt(value));
+    }
+
+    if (field.includes("-")) {
+      const [start, end] = field.split("-").map((value) => parseInt(value));
+      const result = [];
+      for (let i = start; i <= end; i++) {
+        result.push(i);
+      }
+      return result;
+    }
+
+    if (field.includes("/")) {
+      const [start, addAmount] = field
+        .split("/")
+        .map((value) => parseInt(value));
+      const result = [];
+      for (let i = 0; i < max; i += addAmount) {
+        result.push(i);
+      }
+      return result;
+    }
+
+    return [field];
   };
+
+  const scheduledMinutes = expandField(minute, 60);
+  const scheduledHours = expandField(hour, 24);
+  const scheduledDayOfMonths = expandField(dayOfMonth, 31);
+  const scheduledMonths = expandField(month, 12);
+  const scheduledDayOfWeeks = expandField(dayOfWeek, 7);
+
+  // 3. Print expanded fields
+  console.log(`minute        ${scheduledMinutes.join(" ")}`);
+  console.log(`hour          ${scheduledHours.join(" ")}`);
+  console.log(`day of month  ${scheduledDayOfMonths.join(" ")}`);
+  console.log(`month         ${scheduledMonths.join(" ")}`);
+  console.log(`day of week   ${scheduledDayOfWeeks.join(" ")}`);
+  console.log(`command       ${command.join(" ")}`);
 };
+
+const cronString = process.argv[2];
+
+cronParser(cronString);
 
 module.exports = { cronParser };
--- cron-parser.test.js
+++ cron-parser.test.js
@@ -1,17 +1,32 @@
 const { cronParser } = require("./cron-parser");
 
 describe("cronParser", () => {
+  let consoleLogSpy;
+
+  beforeEach(() => {
+    consoleLogSpy = jest.spyOn(console, "log").mockImplementation();
+  });
+
+  afterEach(() => {
+    consoleLogSpy.mockRestore();
+  });
+
   it("parses a cron string", () => {
     const cronString = "*/15 0 1,15 * 1-5 /usr/bin/find";
-    const expected = {
-      minute: "*/15",
-      hour: "0",
-      dayOfMonth: "1,15",
-      month: "*",
-      dayOfWeek: "1-5",
-      command: "/usr/bin/find",
-    };
-    const actual = cronParser(cronString);
-    expect(actual).toEqual(expected);
+    const expected = [
+      "minute        0 15 30 45",
+      "hour          0",
+      "day of month  1 15",
+      "month         1 2 3 4 5 6 7 8 9 10 11 12",
+      "day of week   1 2 3 4 5",
+      "command       /usr/bin/find",
+    ];
+    cronParser(cronString);
+    expect(consoleLogSpy).toHaveBeenCalledWith(expected[0]);
+    expect(consoleLogSpy).toHaveBeenCalledWith(expected[1]);
+    expect(consoleLogSpy).toHaveBeenCalledWith(expected[2]);
+    expect(consoleLogSpy).toHaveBeenCalledWith(expected[3]);
+    expect(consoleLogSpy).toHaveBeenCalledWith(expected[4]);
+    expect(consoleLogSpy).toHaveBeenCalledWith(expected[5]);
   });
 });
